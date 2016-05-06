605280cf78ffebf0ac3f2a6947e78bab0cef4949
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2016-05-05 22:49:58 -0400

    Added an ability to create comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,17 @@ class PostsController < ApplicationController
     @posts = Post.all
     @new_post = Post.new
   end
+
+  def create
+    @new_post = Post.new(post_params)
+    @new_post.save
+
+    redirect_to root_path 
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:comment)
+  end
 end
